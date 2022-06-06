911f5bcaaa731cf51edd5ec8bb8bc20055992caa
vinaysharma7503 <vinaysharma.7503@gmail.com>
2022-06-06 08:46:04 +0530

    interceptor created

--- src/RootService/index.ts
+++ src/RootService/index.ts
@@ -7,6 +7,36 @@ const RootService: any = (env = 'dev') => env === 'dev' ? Dev : Prod;
 // ENVIRONMENT CHANGE
 const { BASEURL = '' } = RootService();
 
+(()=> {
+
+    axios.interceptors.request.use(function (config) {
+        console.log(config);
+        config.headers= {"Authorization" : `Bearer ${localStorage.getItem('token')}`} 
+        // Do something before request is sent
+        return config;
+      }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+      });
+    
+    // Add a response interceptor
+    axios.interceptors.response.use(function (response) {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response;
+      }, function (error) {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        return Promise.reject(error);
+      });
+
+
+
+
+})();
+
+
+
 export const getReq = async (url: string) => {
 
     return axios.get(BASEURL + url)
